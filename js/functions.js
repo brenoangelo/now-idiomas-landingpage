85e8ad6153ca56eb908377a77d0272ffec019ec2
brenoangelo <brenoangeloal@gmail.com>
2021-07-10 18:35:05 -0300

    finalizado a lp

--- js/functions.js
+++ js/functions.js
@@ -3,8 +3,8 @@ $('.container-slider').slick({
     dots: true,
     arrows: false,
     speed: 1000,
-    slidesToShow: 2,
-    slidesToScroll: 2,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover:false,
@@ -18,3 +18,27 @@ $('.container-slider').slick({
                 }
     ]
 });
+
+/* */
+
+/*ACCORDEON */
+    let accordeonButton = document.querySelectorAll('.accordeon-title')
+    let accordeonContent = document.querySelectorAll('.accordeon-single > p')
+    let accordeonArrow = document.querySelectorAll('.accordeon-title i')
+    accordeonButton.forEach((value, index)=>{
+        accordeonButton[index].addEventListener('click', ()=>{
+            let accContent = accordeonContent[index]
+            let accArrow = accordeonArrow[index]
+            if(accContent.style.display === "block"){
+                accContent.style.display = "none"
+                accArrow.classList.remove('fa-chevron-up')
+                accArrow.classList.add('fa-chevron-down')
+            }else if(accContent.style !== "block"){
+                accContent.style.display = "block"
+                accArrow.classList.remove('fa-chevron-down')
+                accArrow.classList.add('fa-chevron-up')
+            }
+
+        })
+    })
+/* */
